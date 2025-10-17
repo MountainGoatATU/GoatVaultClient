988f12ef9a802e368d2f8c44e0818c4036be0b2b
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-10-17 17:55:17 +0100

    sample argon2 hashing

--- GoatVaultClient/MainWindow.xaml.cs
+++ GoatVaultClient/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Isopoh.Cryptography.Argon2;
+using Isopoh.Cryptography.SecureArray;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace GoatVaultClient
 {
     /// <summary>
@@ -23,6 +29,45 @@ namespace GoatVaultClient
         public MainWindow()
         {
             InitializeComponent();
+            CreateVault();
+        }
+
+        private static readonly RandomNumberGenerator Rng = System.Security.Cryptography.RandomNumberGenerator.Create();
+
+        private void CreateVault() 
+        {
+            var password = "password1";
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[16];
+
+            Rng.GetBytes(salt);
+
+            var config = new Argon2Config
+            {
+                Type = Argon2Type.DataIndependentAddressing,
+                Version = Argon2Version.Nineteen,
+                TimeCost = 10,
+                MemoryCost = 32768,
+                Lanes = 5,
+                Threads = Environment.ProcessorCount, // higher than "Lanes" doesn't help (or hurt)
+                Password = passwordBytes,
+                Salt = salt, // >= 8 bytes if not null
+                //Secret = secret, // from somewhere
+                //AssociatedData = associatedData, // from somewhere
+                HashLength = 20 // >= 4
+            };
+            var argon2A = new Argon2(config);
+            string hashString;
+            using (SecureArray<byte> hashA = argon2A.Hash())
+            {
+                hashString = config.EncodeString(hashA.Buffer);
+            }
+
+            if (Argon2.Verify(hashString, passwordBytes, 5))
+            {
+                // verified
+                Console.WriteLine("Verified");
+            }
         }
     }
 }
