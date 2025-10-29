173101f85b584ac6424cd6e9e41063461c013919
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-10-29 18:08:04 +0000

    vault payload adjusted

--- GoatVaultClient/Models/VaultPayload.cs
+++ GoatVaultClient/Models/VaultPayload.cs
@@ -8,11 +8,14 @@ namespace GoatVaultClient.Models
 {
     public class VaultPayload
     {
+        public string _id { get; set; }
         public string user_id { get; set; }
         public string name { get; set; }
         public string salt { get; set; }
         public string nonce { get; set; }
         public string encrypted_blob { get; set; }
         public string auth_tag { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
     }
 }
