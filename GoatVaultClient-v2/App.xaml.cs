3334e8116dd078aff825e348363004f81ba72e32
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-10-31 17:45:08 +0000

    maui project added

---
+++ GoatVaultClient-v2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GoatVaultClient_v2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GoatVaultClient-v2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GoatVaultClient_v2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
