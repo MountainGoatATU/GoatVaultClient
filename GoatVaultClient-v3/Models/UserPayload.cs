9e6b4217a7d69f003232ff00ac6b0137f6034480
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-11-19 17:35:47 +0000

    models modified

--- GoatVaultClient-v3/Models/UserPayload.cs
+++ GoatVaultClient-v3/Models/UserPayload.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace GoatVaultClient_v3.Models
 {
     public class UserPayload
     {
-        public string? Email { get; set; }
-        public string? Salt { get; set; }
-        public string? Password_hash { get; set; }
-        public bool? Mfa_enabled { get; set; }
-        public string? Mfa_secret { get; set; }
+        [JsonPropertyName("_id")] public string? Id { get; set; }
+        [JsonPropertyName("email")] public string? Email { get; set; }
+        [JsonPropertyName("auth_salt")] public string? Auth_Salt { get; set; }
+        [JsonPropertyName("auth_verifier")] public string? Auth_Verifier { get; set; }
+        [JsonPropertyName("vault")] public VaultPayload? Vault { get; set; }
+        [JsonPropertyName("mfa_enabled")] public bool? Mfa_enabled { get; set; }
+        [JsonPropertyName("mfa_secret")] public string? Mfa_secret { get; set; }
     }
 }
