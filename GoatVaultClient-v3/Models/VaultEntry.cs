2c2f3b64e3016051eeffea29abd4d0db3eea487b
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-11-25 09:42:23 +0000

    vault entry model adjusted

--- GoatVaultClient-v3/Models/VaultEntry.cs
+++ GoatVaultClient-v3/Models/VaultEntry.cs
@@ -11,5 +11,6 @@ namespace GoatVaultClient_v3.Models
         public string Site { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Category { get; set; }
     }
 }
