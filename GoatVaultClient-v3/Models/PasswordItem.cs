181a5eb33c0fa7b50f549f7d941a4db1b66fe12b
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-25 20:48:32 +0000

    Added models to display list items

---
+++ GoatVaultClient-v3/Models/PasswordItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoatVaultClient_v3.Models
+{
+    public class PasswordItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; } = "\uf084";
+        public string MaskedPassword => "••••••••••••";
+    }
+}
