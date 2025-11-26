e1ce12c9efac6c128f6ed39f8bb50e6b2c877b04
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-26 20:02:07 +0000

    Added UserResponse

---
+++ GoatVaultClient-v3/Models/UserResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GoatVaultClient_v3.Models
+{
+    public class UserResponse
+    {
+        // Map "_id" from JSON to "Id" in C#
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("auth_salt")]
+        public string AuthSalt { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("mfa_enabled")]
+        public bool MfaEnabled { get; set; }
+
+        [JsonPropertyName("vault")]
+        public VaultPayload Vault { get; set; }
+    }
+}
