c3844a175b7900957cd76b9aba6a6f4700ce93eb
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-26 21:30:33 +0000

    Decrypted vault gets deleted when app loses focus or is put in a background

--- GoatVaultClient-v3/App.xaml.cs
+++ GoatVaultClient-v3/App.xaml.cs
@@ -1,23 +1,31 @@
 ﻿namespace GoatVaultClient_v3
 {
+    using GoatVaultClient_v3.Services;
     using Microsoft.Extensions.DependencyInjection;
 
     public partial class App : Application
     {
         private readonly IServiceProvider _services;
+        private readonly VaultSessionService _sessionService;
         public IServiceProvider Services => _services;
 
-        public App(IServiceProvider services)
+        public App(IServiceProvider services, VaultSessionService sessionService)
         {
             _services = services;
             InitializeComponent();
-            MainPage = services.GetRequiredService<MainPage>();
+            _sessionService = sessionService;
+            //MainPage = services.GetRequiredService<MainPage>();
         }
 
-        //protected override Window CreateWindow(IActivationState? activationState)
-        //{
-        //    var introductionPage = _services.GetRequiredService<IntroductionPage>();
-        //    return new Window(new NavigationPage(introductionPage));
-        //}
+        protected override void OnSleep()
+        {
+            _sessionService.Lock();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var introductionPage = _services.GetRequiredService<IntroductionPage>();
+            return new Window(new NavigationPage(introductionPage));
+        }
     }
 }
