eeb8a87d004891f442c1817a0bc7834d63bc9171
samuel-kocych <samuel@kocych.cz>
2025-11-29 12:07:37 +0000

    removed unused class

--- GoatVaultClient-v3/ViewModels/MainPageViewModel.cs
+++ GoatVaultClient-v3/ViewModels/MainPageViewModel.cs
@@ -349,15 +349,6 @@ namespace GoatVaultClient_v3.ViewModels
         #endregion
     }
 
-    public class InverseBoolConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => !(bool)value;
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => !(bool)value;
-    }
-
     public class EyeIconConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -369,5 +360,4 @@ namespace GoatVaultClient_v3.ViewModels
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => throw new NotImplementedException();
     }
-
 }
