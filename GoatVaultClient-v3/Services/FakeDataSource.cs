e5ef4354d650f703936e53dc59d298b2067c8792
Martin-Strelec <m.strelec2021@gmail.com>
2026-01-14 13:38:42 +0000

    Added FakeData service

--- GoatVaultClient-v3/Services/FakeDataSource.cs
+++ GoatVaultClient-v3/Services/FakeDataSource.cs
@@ -6,20 +6,20 @@ namespace GoatVaultClient_v3.Services
     public class FakeDataSource
     {
         private readonly Faker<VaultEntry> vaultEntryFaker = new Faker<VaultEntry>()
-            .RuleFor(f => f.Site, f => f.Lorem.Word())
-            .RuleFor(f => f.UserName, f => f.Internet.UserName())
+            .RuleFor(f => f.Site, f => f.Company.CompanyName())
+            .RuleFor(f => f.UserName, f => f.Internet.Email())
             .RuleFor(f => f.Password, f => f.Internet.Password())
             .RuleFor(f => f.Description, f => f.Lorem.Sentence())
             .RuleFor(f => f.Category, f => f.PickRandom(new List<string> { "Work", "Personal", "Finance", "Health", "Travel", }));
 
-        public List<FolderItem> GetFolderItems()
+        public List<CategoryItem> GetFolderItems()
         {
             string[] categories = { "Work", "Personal", "Finance", "Health", "Travel", };
-            List<FolderItem> folderItems = new List<FolderItem>();
+            List<CategoryItem> folderItems = new List<CategoryItem>();
             
             foreach (var c in categories)
             {
-                folderItems.Add(new FolderItem { Name = c });
+                folderItems.Add(new CategoryItem { Name = c });
             }
 
             return folderItems;
