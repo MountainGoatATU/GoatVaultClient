ed2e649e4ba981799f265e5ab929605d35e07e58
samuel-kocych <samuel@kocych.cz>
2026-01-18 23:21:31 +0000

    added a service

---
+++ GoatVaultClient-v3/Services/PasswordStrengthService.cs
@@ -0,0 +1,31 @@
+﻿using Zxcvbn;
+
+namespace GoatVaultClient_v3.Services
+{
+    public class PasswordStrengthService
+    {
+        public PasswordCrackInfo Evaluate(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                return new PasswordCrackInfo
+                {
+                    Score = 0,
+                    CrackTimeText = null
+                };
+
+            var result = Core.EvaluatePassword(password);
+
+            return new PasswordCrackInfo
+            {
+                Score = result.Score,
+                CrackTimeText = result.CrackTimeDisplay.OfflineFastHashing1e10PerSecond
+            };
+        }
+    }
+
+    public class PasswordCrackInfo
+    {
+        public int Score { get; set; }
+        public string? CrackTimeText { get; set; }
+    }
+}
