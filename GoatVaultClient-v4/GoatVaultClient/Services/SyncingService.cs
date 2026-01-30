f1feeece6e7ea22b436cf289925c41567f27fb19
Martin-Strelec <m.strelec2021@gmail.com>
2026-01-30 20:05:44 +0000

    Added syncing service

---
+++ GoatVaultClient-v4/GoatVaultClient/Services/SyncingService.cs
@@ -0,0 +1,21 @@
+﻿
+using GoatVaultInfrastructure.Services.Vault;
+using System.Runtime.CompilerServices;
+
+namespace GoatVaultClient.Services
+{
+    public class SyncingService
+    {
+        private readonly VaultService _vaultService;
+        private readonly VaultSessionService _vaultSessionService;
+        private readonly ConnectivityService _connectivityService;
+        public SyncingService(VaultService vaultService, VaultSessionService vaultSessionService, ConnectivityService connectivityService) 
+        {
+            _vaultService = vaultService;
+            _vaultSessionService = vaultSessionService;
+            _connectivityService = connectivityService;
+        }
+
+        
+    }
+}
