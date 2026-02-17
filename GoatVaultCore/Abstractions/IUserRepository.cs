b076107adc8c2b881041adb57a2cb57e0cb7f516
x0rtex <s00253368@atu.ie>
2026-02-17 21:16:16 +0000

    Added GetAllAsync and DeleteAsync actions to user repository

--- GoatVaultCore/Abstractions/IUserRepository.cs
+++ GoatVaultCore/Abstractions/IUserRepository.cs
@@ -1,4 +1,4 @@
-﻿using GoatVaultCore.Models;
+using GoatVaultCore.Models;
 
 namespace GoatVaultCore.Abstractions;
 
@@ -6,5 +6,7 @@ public interface IUserRepository
 {
     Task<User?> GetByEmailAsync(Email email);
     Task<User?> GetByIdAsync(Guid id);
+    Task<List<User>> GetAllAsync();
     Task SaveAsync(User user);
+    Task DeleteAsync(User user);
 }
--- GoatVaultInfrastructure/Database/UserRepository.cs
+++ GoatVaultInfrastructure/Database/UserRepository.cs
@@ -1,4 +1,4 @@
-﻿using GoatVaultCore.Abstractions;
+using GoatVaultCore.Abstractions;
 using GoatVaultCore.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,9 +10,24 @@ public sealed class UserRepository(AppDbContext db) : IUserRepository
 
     public Task<User?> GetByIdAsync(Guid id) => db.Users.FirstOrDefaultAsync(u => u.Id == id);
 
+    public Task<List<User>> GetAllAsync() => db.Users.ToListAsync();
+
     public async Task SaveAsync(User user)
     {
-        db.Users.Update(user);
+        if (db.Users.Any(u => u.Id == user.Id))
+        {
+            db.Users.Update(user);
+        }
+        else
+        {
+            db.Users.Add(user);
+        }
         await db.SaveChangesAsync();
+    }
+
+    public Task DeleteAsync(User user)
+    {
+        db.Users.Remove(user);
+        return db.SaveChangesAsync();
     }
 }
