34a716486b5c06a0437662576c3ebc8cf2eed8f4
x0rtex <s00253368@atu.ie>
2026-02-25 15:44:29 +0000

    Create argon 2 parameters record to be passed into user model

---
+++ GoatVaultCore/Models/Objects/Argon2Parameters.cs
@@ -0,0 +1,10 @@
+﻿namespace GoatVaultCore.Models.Objects;
+
+public sealed record Argon2Parameters
+{
+    public required string Type { get; set; } // "id"
+    public required int MemoryCost { get; set; }
+    public required int TimeCost { get; set; }
+    public required int Lanes { get; set; }
+    public required int Version { get; set; }
+}
