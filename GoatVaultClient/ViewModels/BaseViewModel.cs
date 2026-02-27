d960916674d406b7e584dc20696839a33ba45822
Martin-Strelec <m.strelec2021@gmail.com>
2026-02-27 14:25:10 +0000

    fixed some of the binding issues

--- GoatVaultClient/ViewModels/BaseViewModel.cs
+++ GoatVaultClient/ViewModels/BaseViewModel.cs
@@ -7,7 +7,9 @@ namespace GoatVaultClient.ViewModels;
 
 public partial class BaseViewModel : ObservableObject
 {
-    [ObservableProperty][NotifyPropertyChangedFor(nameof(IsNotBusy))] private bool _isBusy;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))] 
+    private bool _isBusy;
     [ObservableProperty] private string _title = string.Empty;
 
     // Helper property for XAML (e.g., IsEnabled="{Binding IsNotBusy}")
