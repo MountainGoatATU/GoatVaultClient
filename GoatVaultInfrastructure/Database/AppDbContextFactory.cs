1a0dc7352e8b6bca9483c7582aa2e0769ea83139
x0rtex <s00253368@atu.ie>
2026-02-27 00:49:27 +0000

    Ignore unused AppDbContextFactory

--- GoatVaultInfrastructure/Database/AppDbContextFactory.cs
+++ GoatVaultInfrastructure/Database/AppDbContextFactory.cs
@@ -3,12 +3,13 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace GoatVaultInfrastructure.Database;
 
+// ReSharper disable once UnusedMember.Global
 public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        var dbPath = Path.Combine(AppContext.BaseDirectory, "goatvault-design.db");
+        var dbPath = Path.Combine(AppContext.BaseDirectory, "goatvault-migrations.db");
         optionsBuilder.UseSqlite($"Data Source={dbPath}");
         return new AppDbContext(optionsBuilder.Options);
     }
