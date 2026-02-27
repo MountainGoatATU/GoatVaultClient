b2c49d91521724ca5a69e7faa0241cabeb9afde3
x0rtex <s00253368@atu.ie>
2026-02-27 15:32:02 +0000

    Added delete account use case

---
+++ GoatVaultApplication/Account/DeleteAccountUseCase.cs
@@ -0,0 +1,34 @@
+﻿using GoatVaultApplication.Auth;
+using GoatVaultCore.Abstractions;
+
+namespace GoatVaultApplication.Account;
+
+public class DeleteAccountUseCase(
+    ISessionContext session,
+    IUserRepository users,
+    ICryptoService crypto,
+    IServerAuthService serverAuth,
+    LogoutUseCase logout)
+{
+    public async Task ExecuteAsync(string currentPassword)
+    {
+        if (session.UserId == null)
+            throw new InvalidOperationException("No user logged in.");
+
+        var user = await users.GetByIdAsync(session.UserId.Value)
+                   ?? throw new InvalidOperationException("User not found.");
+
+        // Verify current password
+        var currentAuthVerifier = await Task.Run(() => crypto.GenerateAuthVerifier(currentPassword, user.AuthSalt, user.Argon2Parameters));
+        if (!currentAuthVerifier.SequenceEqual(user.AuthVerifier))
+        {
+            throw new UnauthorizedAccessException("Incorrect current password.");
+        }
+
+        // Delete request
+        await serverAuth.DeleteUserAsync(user.Id);
+
+        // Logout
+        await logout.ExecuteAsync();
+    }
+}
