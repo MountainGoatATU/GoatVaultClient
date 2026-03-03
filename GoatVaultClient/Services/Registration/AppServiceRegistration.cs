11fd28b184170e09359f410c758de372de62e023
Martin-Strelec <m.strelec2021@gmail.com>
2026-03-03 21:47:45 +0000

    Added service registration classes

---
+++ GoatVaultClient/Services/Registration/AppServiceRegistration.cs
@@ -0,0 +1,72 @@
+﻿using GoatVaultApplication.Session;
+using GoatVaultClient.Pages;
+using GoatVaultClient.ViewModels;
+using GoatVaultClient.ViewModels.controls;
+using GoatVaultClient.ViewModels.Controls;
+using GoatVaultCore.Abstractions;
+using GoatVaultCore.Services;
+using GoatVaultInfrastructure.Database;
+using GoatVaultInfrastructure.Services;
+using GoatVaultInfrastructure.Services.Api;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xecrets.Slip39;
+
+namespace GoatVaultClient.Services.Registration;
+
+public static class AppServiceRegistration
+{
+    public static IServiceCollection AddAppServices(this IServiceCollection services)
+    {
+        // Core app services
+        services.AddSingleton<ConnectivityService>();
+        services.AddSingleton<ISyncingService, SyncingService>();
+        services.AddSingleton<ISessionContext, SessionContext>();
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddSingleton<ICryptoService, CryptoService>();
+        services.AddSingleton<IVaultCrypto, VaultCrypto>();
+        services.AddSingleton<IAuthTokenService, AuthTokenService>();
+        services.AddSingleton<JwtUtils>();
+
+        // Misc services
+        services.AddSingleton<GoatTipsService>();
+        services.AddSingleton<TotpManagerService>();
+        services.AddTransient<CategoryManagerService>();
+        services.AddTransient<VaultEntryManagerService>();
+        services.AddTransient<IPwnedPasswordService, PwnedPasswordService>();
+        services.AddTransient<IVaultScoreCalculatorService, VaultScoreCalculatorService>();
+        services.AddTransient<IPasswordStrengthService, PasswordStrengthService>();
+        services.AddTransient<IRandomTipService, RandomTipService>();
+
+        // Shamir Services
+        services.AddTransient<IShamirsSecretSharing, ShamirsSecretSharing>();
+        services.AddSingleton<IRandom, StrongRandom>();
+        services.AddTransient<IShamirSsService, ShamirSsService>();
+
+        // HTTP services
+        services.AddTransient<AuthenticatedHttpHandler>(sp =>
+        {
+            const string refreshEndpoint = "v1/auth/refresh";
+            var serverBaseUrl = sp.GetRequiredService<IConfiguration>()["API_BASE_URL"];
+            var authService = sp.GetRequiredService<IAuthTokenService>();
+            var jwtUtils = sp.GetRequiredService<JwtUtils>();
+            var logger = sp.GetService<ILogger<AuthenticatedHttpHandler>>();
+            return new AuthenticatedHttpHandler(
+                authService,
+                jwtUtils,
+                $"{serverBaseUrl}/{refreshEndpoint}",
+                logger
+            );
+        });
+
+        services.AddHttpClient<IServerAuthService, ServerAuthService>()
+            .AddHttpMessageHandler<AuthenticatedHttpHandler>();
+        services.AddHttpClient<IHttpService, HttpService>()
+            .AddHttpMessageHandler<AuthenticatedHttpHandler>();
+
+        return services;
+    }
+}
---
+++ GoatVaultClient/Services/Registration/UseCasesServiceRegistration.cs
@@ -0,0 +1,49 @@
+﻿using GoatVaultApplication.Account;
+using GoatVaultApplication.Auth;
+using GoatVaultApplication.Shamir;
+using GoatVaultApplication.Vault;
+using GoatVaultClient.Pages;
+using GoatVaultClient.ViewModels;
+using GoatVaultClient.ViewModels.controls;
+using GoatVaultClient.ViewModels.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoatVaultClient.Services.Registration;
+
+public static class UseCasesServiceRegistration
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        // Login & Account Use Cases
+        services.AddTransient<LoginOfflineUseCase>();
+        services.AddTransient<LoginOnlineUseCase>();
+        services.AddTransient<LogoutUseCase>();
+        services.AddTransient<RegisterUseCase>();
+        // Shamir Use Cases
+        services.AddTransient<DisableShamirUseCase>();
+        services.AddTransient<EnableShamirUseCase>();
+        services.AddTransient<RecoverKeyUseCase>();
+        services.AddTransient<SplitKeyUseCase>();
+        services.AddTransient<ValidateUserEmailUseCase>();
+        // Vault Use Cases
+        services.AddTransient<AddVaultEntryUseCase>();
+        services.AddTransient<CalculateVaultScoreUseCase>();
+        services.AddTransient<DeleteVaultEntryUseCase>();
+        services.AddTransient<LoadVaultUseCase>();
+        services.AddTransient<SaveVaultUseCase>();
+        services.AddTransient<SyncVaultUseCase>();
+        services.AddTransient<UpdateVaultEntryUseCase>();
+        services.AddTransient<WipeVaultUseCase>();
+        // Account Management Use Cases
+        services.AddTransient<ChangeEmailUseCase>();
+        services.AddTransient<ChangePasswordUseCase>();
+        services.AddTransient<DisableMfaUseCase>();
+        services.AddTransient<EnableMfaUseCase>();
+        services.AddTransient<LoadUserProfileUseCase>();
+        services.AddTransient<DeleteAccountUseCase>();
+
+        return services;
+    }
+}
